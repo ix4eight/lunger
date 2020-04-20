152aa15991f7c5817f996222a57d2b1ef69a95fe
ix4eight <ix4eight@gmail.com>
2020-04-21 00:57:53 +0900

    imageuploder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
+  resources :posts do
+    resources :comments, only: [:new, :create]
+  end
+  resources :languages, only: :show 
+  resources :users, only: :show
 end
