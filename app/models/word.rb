43f03459717bb0f9f13d379905a3866189bb3433
ix4eight <ix4eight@gmail.com>
2020-04-21 01:04:45 +0900

    js update

---
+++ app/models/word.rb
@@ -0,0 +1,11 @@
+class Word < ActiveHash::Base
+  self.data = [
+      {id: 1, name: 'HTML'}, {id: 2, name: 'CSS'}, {id: 3, name: 'Ruby'},
+      {id: 4, name: 'Java'}, {id: 5, name: 'PHP'}, {id: 6, name: 'Python'},
+      {id: 7, name: 'SQL'}, {id: 8, name: 'C'}, {id: 9, name: 'C#'},
+      {id: 10, name: 'C++'}, {id: 11, name: 'Ruby on Rails'}, {id: 12, name: 'Spring'},
+      {id: 13, name: 'CakePHP'}, {id: 14, name: 'AngularJS'}, {id: 15, name: 'DJango'},
+      {id: 16, name: 'Bootstrap'}, {id: 17, name: 'AWS'}, {id: 18, name: 'Git GitHub'},
+      {id: 19, name: 'MySQL'}
+  ]
+end
